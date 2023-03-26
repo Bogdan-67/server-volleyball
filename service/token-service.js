d29f43beed645b379d96d0da8b0389861ec6d3d9
Bogdan-67 <tugay.bogdan03@mail.ru>
2023-03-26 21:19:20 +0300

    fixed saving of tokens in table

--- service/token-service.js
+++ service/token-service.js
@@ -14,11 +14,11 @@ class TokenService {
   async saveToken(accountId, refreshToken) {
     const tokenData = await db.query(`SELECT * FROM tokens WHERE account_id = $1`, [accountId]);
     console.log('tokenData:', tokenData);
-    if (tokenData) {
-      await db.query(`UPDATE tokens SET refresh_token = $1 WHERE account_id = $2`, [
-        refreshToken,
-        accountId,
-      ]);
+    if (tokenData.rows[0]) {
+      tokenData = await db.query(
+        `UPDATE tokens SET refresh_token = $1 WHERE account_id = $2 RETURNING *`,
+        [refreshToken, accountId],
+      );
       return tokenData;
     }
     const token = await db.query(
