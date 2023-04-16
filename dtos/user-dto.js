136351f11a7adb721c9ba2994ae7bdf7c283ea61
Bogdan-67 <tugay.bogdan03@mail.ru>
2023-04-17 00:05:05 +0300

    changed user dto

--- dtos/user-dto.js
+++ dtos/user-dto.js
@@ -6,20 +6,21 @@ module.exports = class UserDTO {
   patronimyc;
   email;
   phone;
-  group;
+  team;
   login;
   role;
 
   constructor(model) {
     this.id_account = model.id_account;
     this.id_user = model.id_user;
+    this.user_id = model.user_id;
     this.name = model.name;
     this.surname = model.surname;
     this.patronimyc = model.patronimyc;
     this.email = model.email;
     this.phone = model.phone;
-    this.group = model.group;
+    this.team = model.team;
     this.login = model.login;
-    this.role = model.name;
+    this.role = model.role_name;
   }
 };
