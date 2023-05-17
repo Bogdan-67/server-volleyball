af986e33d10076f9ae6ed0cace12ac9aab83f4cf
RaKeD1 <121726628+RaKeD1@users.noreply.github.com>
2023-05-17 18:13:12 +0300

    fix name params

--- dtos/selectUsers-dto.js
+++ dtos/selectUsers-dto.js
@@ -3,11 +3,15 @@ module.exports = class SelectUsersDTO {
   name;
   surname;
   patronimyc;
+  email;
+  phone;
 
   constructor(model) {
     this.id_account = model.id_account;
     this.name = model.name;
     this.surname = model.surname;
     this.patronimyc = model.patronimyc;
+    this.email = model.email;
+    this.phone = model.phone;
   }
 };
