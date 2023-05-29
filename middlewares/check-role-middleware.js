f9c152d66ec784d83023f4d58b48a0e0218dc5c3
Bogdan-67 <tugay.bogdan03@mail.ru>
2023-05-29 21:49:22 +0300

    checkRole fix

--- middlewares/check-role-middleware.js
+++ middlewares/check-role-middleware.js
@@ -19,7 +19,9 @@ module.exports = function (role) {
         return next(ApiError.UnauthorisedError());
       }
 
-      if (userData.role !== role || userData.role !== 'ADMIN') {
+      console.log(userData);
+
+      if (userData.role !== role && userData.role !== 'ADMIN') {
         return next(ApiError.BadRequest('Нет прав доступа'));
       }
       req.user = userData;
